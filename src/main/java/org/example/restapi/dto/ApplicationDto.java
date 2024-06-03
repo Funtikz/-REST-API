c07d21072ad50d3523181b5e2459d8f306be8dfa
Nikita Sherbakov <228funtik@gmail.com>
2024-06-03 17:29:19 +0300

    Updated ApplicationDto with validation version 2

--- src/main/java/org/example/restapi/dto/ApplicationDto.java
+++ src/main/java/org/example/restapi/dto/ApplicationDto.java
@@ -1,6 +1,7 @@
 package org.example.restapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -11,18 +12,29 @@ import java.time.LocalDateTime;
 @Data
 @Accessors(chain = true)
 public class ApplicationDto {
-    @NotBlank
+    @NotBlank(message = "Поле должно быть заполнено.")
     @Size(min = 2, max = 64)
-    String firstName;
-    @NotBlank
+    private String firstName;
+
+    @NotBlank(message = "Поле должно быть заполнено.")
     @Size(min = 2, max = 64)
-    String middleName;
-    @NotBlank
+    private String middleName;
+
+    @NotBlank(message = "Поле должно быть заполнено.")
+    @Pattern(regexp = "\\d{10}", message = "Неверно указанный номер телефона.")
+    private String phoneNumber;
+
+    @NotBlank(message = "Поле должно быть заполнено.")
     @Size(min = 2, max = 64)
-    String lastName;
-    @Positive
-    Integer couldCounter;
-    @Positive
-    Integer hotCounter;
-    LocalDateTime orderTime;
+    private String lastName;
+
+    @NotBlank(message = "Поле должно быть заполнено.")
+    @Positive(message = "Количество счетчиков должно быть больше 0")
+    private Integer couldCounter;
+
+    @NotBlank(message = "Поле должно быть заполнено.")
+    @Positive(message = "Количество счетчиков должно быть больше 0")
+    private Integer hotCounter;
+
+    private LocalDateTime orderTime;
 }
