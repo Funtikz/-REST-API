32defc9dd94887e196875eda306515132f52ebe9
Nikita Sherbakov <228funtik@gmail.com>
2024-06-03 16:10:57 +0300

    Added application entity and dependency spring-data

---
+++ src/main/java/org/example/restapi/entity/Application.java
@@ -0,0 +1,25 @@
+package org.example.restapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "application")
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "last_name")
+    private String firstName;
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "could_counter")
+    private Integer couldCounter;
+    @Column(name = "hot_counter")
+    private Integer hotCounter;
+}
