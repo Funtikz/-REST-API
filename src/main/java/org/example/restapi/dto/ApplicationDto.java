5a30fbf195c7f239ac8b58d408a2f102e5bbea57
Nikita Sherbakov <228funtik@gmail.com>
2024-06-03 17:14:22 +0300

    Updated ApplicationDto

--- src/main/java/org/example/restapi/dto/ApplicationDto.java
+++ src/main/java/org/example/restapi/dto/ApplicationDto.java
@@ -3,6 +3,8 @@ package org.example.restapi.dto;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.time.LocalDateTime;
+
 @Data
 @Accessors(chain = true)
 public class ApplicationDto {
@@ -11,4 +13,5 @@ public class ApplicationDto {
     String lastName;
     Integer couldCounter;
     Integer hotCounter;
+    LocalDateTime orderTime;
 }
