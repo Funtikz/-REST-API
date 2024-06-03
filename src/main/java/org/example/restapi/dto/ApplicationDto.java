8be231c61495d5fcacbdb755c4bd3cd6b42f0902
Nikita Sherbakov <228funtik@gmail.com>
2024-06-03 17:18:26 +0300

    Updated ApplicationDto with validation

--- src/main/java/org/example/restapi/dto/ApplicationDto.java
+++ src/main/java/org/example/restapi/dto/ApplicationDto.java
@@ -1,5 +1,8 @@
 package org.example.restapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -8,10 +11,18 @@ import java.time.LocalDateTime;
 @Data
 @Accessors(chain = true)
 public class ApplicationDto {
+    @NotBlank
+    @Size(min = 2, max = 64)
     String firstName;
+    @NotBlank
+    @Size(min = 2, max = 64)
     String middleName;
+    @NotBlank
+    @Size(min = 2, max = 64)
     String lastName;
+    @Positive
     Integer couldCounter;
+    @Positive
     Integer hotCounter;
     LocalDateTime orderTime;
 }
