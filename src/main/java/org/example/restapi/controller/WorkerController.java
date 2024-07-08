a209a782cacb787b96f84818dee359414e3a8bc8
Nikita Sherbakov <228funtik@gmail.com>
2024-07-08 19:53:49 +0300

    Added WorkerController and WorkerCreationRequest

---
+++ src/main/java/org/example/restapi/controller/WorkerController.java
@@ -0,0 +1,47 @@
+package org.example.restapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.restapi.dto.WorkerCreationRequest;
+import org.example.restapi.entity.Worker;
+import org.example.restapi.entity.WorkerCredentials;
+import org.example.restapi.service.model.WorkerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/worker")
+@AllArgsConstructor
+public class WorkerController {
+    private WorkerService workerService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Worker> createWorker(@RequestBody WorkerCreationRequest workerRequest){
+        WorkerCredentials workerCredentials = workerRequest.getWorkerCredentials();
+        Worker worker = workerRequest.getWorker();
+        return new ResponseEntity<>(workerService.createWorker(worker,workerCredentials), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Worker> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(workerService.getById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWorker(@PathVariable("id") Long id ){
+        workerService.deleteWorker(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Worker> updateWorker(@PathVariable("id") Long id,
+                                               @RequestBody Worker worker){
+        return new ResponseEntity<>(workerService.updateWorker(id, worker), HttpStatus.OK);
+    }
+
+//    @PutMapping("/{id}/add-application")
+//    public ResponseEntity<Worker> addApplication(@PathVariable("id") Long id,
+//                                                 @RequestBody ApplicationDto applicationDto){
+//
+//    }
+}
